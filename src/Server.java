71f816fd72a423f968bfe2b18dd522d4ef737167
Chanel Morgan <chanelmorgan05@icloud.com>
2024-01-31 12:43:43 +0000

    Creating the server and displaying the image

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,19 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        // Creating the GUI for the server
         JFrame jFrame = new JFrame("Server");
         jFrame.setSize(400, 400);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +24,22 @@ public class Server {
 
         jFrame.setVisible(true);
 
+        // Creating the server socket
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+        Socket socket = serverSocket.accept();
+
+        InputStream inputStream = socket.getInputStream();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+        BufferedImage bufferedImage = ImageIO.read(bufferedInputStream);
+
+        bufferedInputStream.close();
+        socket.close();
+
+        JLabel jLabelPic = new JLabel(new ImageIcon(bufferedImage));
+        jLabelText.setText("Image Received");
+        jFrame.add(jLabelPic, BorderLayout.CENTER);
 
     }
 }
