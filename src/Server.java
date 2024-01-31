b52f1def99324ed89b737439bda3e5a22c8667a4
Chanel Morgan <chanelmorgan05@icloud.com>
2024-01-31 12:38:47 +0000

    Creating the Jframe

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Server {
 
     public static void main(String[] args) {
-        
+
+        JFrame jFrame = new JFrame("Server");
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel jLabelText = new JLabel("Waiting for image from client...");
+
+        jFrame.add(jLabelText, BorderLayout.SOUTH);
+
+        jFrame.setVisible(true);
+
+
     }
 }
